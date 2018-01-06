2329cbff065972f0a20a7e90729bad193b05dffb
AndreaFerlini <andrea.ferlini@fcssolutions.it>
2018-01-06 13:08:01 +0100

    general bug-fixing, changed structure of add and remove. faster and more reliable

--- main.cpp
+++ main.cpp
@@ -4,113 +4,62 @@
 
 using namespace std;
 
+long edges;
 
 // ADDRANDOMLINK: add a random non-existing link to the graph
-void addRandomLink (long n, int step, int steps, bool debug, string filename, string tmp_filename) {
-    long node_1;
-    long node_2;
+void addRandomLink (int n, int step, int steps, bool debug, string filename, bool **matrix) {
+    int node_1 = 0;
+    int node_2 = 0;
+
     fstream graph;
-    fstream tmp_graph;
-
-    string line;
-    string target_line;
-    string target_line_reverse;
-
-    bool exists = false;
 
     if (debug)
         cout << endl << "[DEBUG] Creating a link ..." << endl;
 
-    tmp_graph.open(filename);
-    if (! tmp_graph.is_open()){
+    // pick for the first time two random nodes
 
-        if (debug)
-            cout << "[ERROR] Impossible to open the temp file" << endl;
-
-        return;
-    }
-
-    node_1 = rand() % (n+1);
-    node_2 = rand() % (n+1);
+    do {
+        node_1 = rand() % (n);
+        node_2 = rand() % (n);
+    } while (node_1 == node_2);
 
     if (debug) {
         cout << "[DEBUG] node_1: " << node_1 << endl;
         cout << "[DEBUG] node_2: " << node_2 << endl;
     }
 
-    // check the non-existence of the link
+    // check the non-existence of the link:
+    // pick two different random nodes if the link exists (true in the matrix)
     // ******************************************************************
 
-    target_line = to_string(node_1) + " " + to_string(node_2);
-    target_line_reverse = to_string(node_2) + " " + to_string(node_1);
+    if (debug)
+        cout << "matrix: " << matrix[node_1][node_2] << endl;
 
-    if (debug){
-        cout << "[DEBUG] target line: " << target_line << endl;
-        cout << "[DEBUG] target line reverse: " << target_line_reverse << endl;
-    }
+    while (matrix[node_1][node_2] || matrix[node_2][node_1]) {
 
-    while (getline(tmp_graph, line)) {
-        exists = false;
-        //cout << "LINE: " << line << endl;
+        if (debug)
+            cout << "[DEBUG] the link exists" << endl;
 
-        if (line.compare(target_line) == 0 || line.compare(target_line_reverse) == 0 ){
-            exists = true;
-            if (debug)
-                cout << "[DEBUG] the link exists" << endl;
-
-            break;
-        }
-    }
-    tmp_graph.clear();
-    tmp_graph.seekg(0, ios::beg);
-
-    // slow if there are a lot of links
-    while (exists) {
-        exists = false;
-
-        node_1 = rand() % (n+1);
-        node_2 = rand() % (n+1);
+        do {
+            node_1 = rand() % (n);
+            node_2 = rand() % (n);
+        } while (node_1 == node_2);
 
         if (debug) {
             cout << "[DEBUG] node_1: " << node_1 << endl;
             cout << "[DEBUG] node_2: " << node_2 << endl;
         }
-
-        target_line = to_string(node_1) + " " + to_string(node_2);
-        target_line_reverse = to_string(node_2) + " " + to_string(node_1);
-
-        if (debug){
-            cout << "[DEBUG] target line: " << target_line << endl;
-            cout << "[DEBUG] target line reverse: " << target_line_reverse << endl;
-        }
-
-        while (getline(tmp_graph, line)) {
-            if (line.compare(target_line) == 0 || line.compare(target_line_reverse) == 0 ){
-                exists = true;
-            }
-        }
-        tmp_graph.clear();
-        tmp_graph.seekg(0, ios::beg);
-
     }
-    tmp_graph.close();
+
     // ******************************************************************
 
-    //add the link to the file
+    // add the link to the file
+    // ------------------------------------------------------------------
+
     graph.open(filename, fstream::app);
     if (! graph.is_open()){
 
-        if (debug)
-            cout << "[ERROR] Impossible to open the file" << endl;
-
-        return;
-    }
-
-    tmp_graph.open(tmp_filename, fstream::out | fstream::app);
-    if (! tmp_graph.is_open()){
-
-        if (debug)
-            cout << "[ERROR] Impossible to open the temp file" << endl;
+        cout << "[ERROR] Impossible to open the file" << endl;
 
         return;
     }
@@ -118,44 +67,37 @@ void addRandomLink (long n, int step, int steps, bool debug, string filename, st
     if (debug)
         cout << "[DEBUG] adding: " << node_1 << "-" << node_2 << endl;
 
-    tmp_graph << node_1 << " " << node_2 << endl;
-    graph << node_1 << " " << node_2 << " " << steps-step << " C\n";
+    graph << steps-step << " " << node_1 << " " << node_2 << " C\n";
 
-    tmp_graph.close();
+    // updating the matrix after adding the link
+    matrix[node_1][node_2] = true;
+
+    // incrementing the global variable
+    edges ++;
+
     graph.close();
+
+    // ------------------------------------------------------------------
+
     if (debug)
         cout << endl;
 }
 
 
 // REMOVERANDOMLINK: remove a random existing link from the graph
-void removeRandomLink (long n, int step, int steps, bool debug, string filename, string tmp_filename, string log_tmp_filename) {
-    long node_1;
-    long node_2;
+void removeRandomLink (int n, int step, int steps, bool debug, string filename, bool **matrix) {
+    int node_1=0;
+    int node_2=0;
+
     fstream graph;
-    fstream tmp_graph;
-    fstream log_tmp_graph;
-
-    string line;
-    string target_line;
-    string target_line_reverse;
-
-    bool exists = false;
 
     if (debug)
         cout << endl << "[DEBUG] Deleting a link ..." << endl;
 
-    tmp_graph.open(tmp_filename);
-    if (! tmp_graph.is_open()){
+    // pick for the first time two random nodes
 
-        if (debug)
-            cout << "[ERROR] Impossible to open the temp file" << endl;
-
-        return;
-    }
-
-    node_1 = long (rand() % (n+1));
-    node_2 = long (rand() % (n+1));
+    node_1 = int (rand() % (n));
+    node_2 = int (rand() % (n));
 
 
     if (debug) {
@@ -163,164 +105,108 @@ void removeRandomLink (long n, int step, int steps, bool debug, string filename,
         cout << "[DEBUG] node_2: " << node_2 << endl;
     }
 
-    // check the existence of the link
+    // check the existence of the link:
+    // pick two different random nodes if the link does not exist (false in the matrix)
+    // ******************************************************************
 
-    target_line = to_string(node_1) + " " + to_string(node_2);
-    target_line_reverse = to_string(node_2) + " " + to_string(node_1);
+    if (debug)
+        cout << "matrix: " << matrix[node_1][node_2] << endl;
 
-    if (debug) {
-        cout << "[DEBUG] target line: " << target_line << endl;
-        cout << "[DEBUG] target line reverse: " << target_line_reverse << endl;
-    }
+    while (!matrix[node_1][node_2] && !matrix[node_2][node_1]) {
 
-    while (getline(tmp_graph, line)) {
-        if (line.compare(target_line) == 0 || line.compare(target_line_reverse) == 0 ){
-            exists = true;
-        }
-    }
-    tmp_graph.clear();
-    tmp_graph.seekg(0, ios::beg);
+        if (debug)
+            cout << "[DEBUG] the link exists" << endl;
 
-    while (!exists) {
-        exists = false;
-
-        node_1 = rand() % (n+1);
-        node_2 = rand() % (n+1);
+        do {
+            node_1 = rand() % (n);
+            node_2 = rand() % (n);
+        } while (node_1 == node_2);
 
         if (debug) {
-
             cout << "[DEBUG] node_1: " << node_1 << endl;
             cout << "[DEBUG] node_2: " << node_2 << endl;
-
         }
-
-        target_line = to_string(node_1) + " " + to_string(node_2);
-        target_line_reverse = to_string(node_2) + " " + to_string(node_1);
-
-        if (debug) {
-            cout << "[DEBUG] target line: " << target_line << endl;
-            cout << "[DEBUG] target line reverse: " << target_line_reverse << endl;
-        }
-
-        while (getline(tmp_graph, line)) {
-            if (line.compare(target_line) == 0 || line.compare(target_line_reverse) == 0 ){
-                exists = true;
-                break;
-            }
-        }
-        tmp_graph.clear();
-        tmp_graph.seekg(0, ios::beg);
     }
+
+    // ******************************************************************
 
     // remove the link from the file
+    // ------------------------------------------------------------------
 
-    graph.open(filename, fstream::out | fstream::app);
+    graph.open(filename, fstream::app);
     if (! graph.is_open()){
 
-        if (debug)
-            cout << "[ERROR] Impossible to open the file" << endl;
+        cout << "[ERROR] Impossible to open the file" << endl;
 
         return;
     }
 
-    log_tmp_graph.open(log_tmp_filename, fstream::out | fstream::app);
-    if (! log_tmp_graph.is_open()){
+    if (matrix[node_1][node_2]){
+        graph << steps-step+1 << " " << node_1 << " " << node_2 << " D\n";
 
-        if (debug)
-            cout << "[ERROR] Impossible to open the log temp file" << endl;
+        // updating the matrix after removing the link
+        matrix[node_1][node_2] = false;
+    } else {
+        graph << steps-step+1 << " " << node_2 << " " << node_1 << " D\n";
 
-        return;
+        // updating the matrix after removing the link
+        matrix[node_2][node_1] = false;
     }
 
-    while (getline(tmp_graph, line)) {
-        if (line.compare(target_line) == 0 || line.compare(target_line_reverse) == 0 ){
-            graph << node_1 << " " << node_2 << " " << steps-step+1 << " D\n";
-        }
-        if (line.compare(target_line) != 0 && line.compare(target_line_reverse) != 0 ){
-            log_tmp_graph << line << endl;
-        }
-    }
-
-    tmp_graph.clear();
-    tmp_graph.seekg(0, ios::beg);
-
-    const char * c_tmp_filename = tmp_filename.c_str();
-    const char * c_log_tmp_filename = log_tmp_filename.c_str();
-
-    std::rename(c_tmp_filename , "../log.graph");
-    std::rename(c_log_tmp_filename, c_tmp_filename);
+    // decrementing the global variable
+    edges --;
 
     graph.close();
-    tmp_graph.close();
-    log_tmp_graph.close();
+
+    // ------------------------------------------------------------------
 
     if (debug)
         cout << endl;
 }
 
 
-//READFILE
-void readFile (bool debug, string filename) {
-    string line;
-    fstream graph;
-
-    cout << endl << "[STATUS] Printing the graph ..." << endl;
-
-    graph.open(filename);
-    if (! graph.is_open()){
-
-        if (debug)
-            cout << "[ERROR] Impossible to open the file" << endl;
-
-        return;
-    }
-    while (getline(graph, line)) {
-        cout << "[OUT] " << line << endl;
-    }
-    graph.clear();
-    graph.seekg(0, ios::beg);
-
-    graph.close();
-}
-
-
 // EDGEMARKOVIAN: the core of the evolving edge-markovian model
-void edgeMarkovian (long n, float p, float q, int steps, bool debug, string filename, string tmp_filename, string log_tmp_filename){
-    int prob_creation;
-    int prob_deletion;
+void edgeMarkovian (int n, double p, double q, int steps, bool debug, string filename){
+    double prob_creation = 0;
+    double prob_deletion = 0;
 
     int max_steps = steps;
 
     ofstream graph;
-    ofstream tmp_graph;
 
-    //ROOM FOR IMPROVEMENTS: load the graph in memory as an adjList
+    bool **links_matrix = new bool *[n]; // rows
+
+    long max_num_edges = ((n-1)*n)/2;
+    long non_existing_edges = max_num_edges;
+
+    int created=0;
+    int deleted=0;
+
+    edges = 0;
+
+    // creation of the matrix (n*n)
+    for (int i = 0; i < n; i ++) {
+        links_matrix[i] = new bool [n]; // columns
+    }
+
+    // init the matrix to false
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            links_matrix[i][j] = false;
+        }
+    }
 
     graph.open (filename);
     if (! graph.is_open()){
 
-        if (debug)
-            cout << "[ERROR] Impossible to open the file" << endl;
-
-        return;
-    }
-
-    tmp_graph.open(tmp_filename);
-    if (! tmp_graph.is_open()){
-
-        if (debug)
-            cout << "[ERROR] Impossible to open the tmp file" << endl;
+        cout << "[ERROR] Impossible to open the file" << endl;
 
         return;
     }
 
     graph.close();
-    tmp_graph.close();
 
     srand(time(0));
-
-    p = p * 100;
-    q = q * 100;
 
     if (debug) {
         cout << "p = " << p << endl;
@@ -328,52 +214,82 @@ void edgeMarkovian (long n, float p, float q, int steps, bool debug, string file
     }
 
     while (steps > 0) {
-        prob_creation = rand() % 100;
-        prob_deletion = rand() % 100;
 
-        if (debug) {
-            cout << "prob_creation = " << prob_creation << endl;
-            cout << "prob_deletion = " << prob_deletion << endl;
+        non_existing_edges = max_num_edges - edges;
+
+        if (non_existing_edges == 0) {
+            cout << "[STATUS] All the edges have been created" << endl;
         }
 
-        if (prob_creation < p) {
-            addRandomLink (n, steps, max_steps, debug, filename, tmp_filename);
+        for (int j = 0; j < non_existing_edges; j ++) {
+            prob_creation = ((double) rand() / (RAND_MAX));
+            if (prob_creation < p) {
+                addRandomLink(n, steps, max_steps, debug, filename, &*links_matrix);
+                created ++;
+            }
         }
 
-        if (prob_deletion < q) {
-            removeRandomLink (n, steps, max_steps, debug, filename, tmp_filename, log_tmp_filename);
+        long e = edges; // e is used to debug, can be replaced with edges
+
+        for (int i = 0; i < e; i ++) {
+            prob_deletion = ((double) rand() / (RAND_MAX));
+            if (prob_deletion < q) {
+                removeRandomLink (n, steps, max_steps, debug, filename, &*links_matrix);
+                deleted ++;
+            }
         }
+
+        cout << "t = " << max_steps - steps << endl;
+        cout << "created links = " << created << endl;
+        cout << "deleted links = " << deleted << endl;
+        cout << "links number = " << edges << endl;
+        cout << endl;
         steps --;
     }
+
+    // free the memory
+    for (int i = 0; i < n; ++i){
+        delete [] links_matrix[i];
+    }
+    delete [] links_matrix;
 }
 
 
 // MAIN: everything is tested in this function
 int main() {
-    long nodes;
-    float p, q;
+    int nodes;
+    double p, q;
     int steps;
     bool debug;
 
     string filename;
-    string tmp_filename;
-    string log_tmp_filename;
 
     filename = "../edgeMarkovian.graph";
-    tmp_filename = "../tmp_edgeMarkovian.graph";
-    log_tmp_filename = "../log_edgeMarkovian.graph";
 
-    nodes = 100;
-    p = 0.8;
-    q = 0.2;
-    steps = 1000;
+/*
+ * ROLLER
+ * */
+/*
+    nodes = 62;
+    p = 0.00322543;
+    q = 0.144295;
+    steps = 9977;
+*/
 
-    debug = true;
+/*
+ * INFOCOM
+ * */
+
+    nodes = 98;
+    p = 9.54057e-05;
+    q = 0.0166691;
+    steps = 337419;
+
+    debug = false;
 
     cout << "[STATUS] Running the program ..." << endl;
 
-    edgeMarkovian(nodes, p, q, steps, debug, filename, tmp_filename, log_tmp_filename);
-    //readFile(debug, filename);
+    edgeMarkovian(nodes, p, q, steps, debug, filename);
 
     return 0;
 }
